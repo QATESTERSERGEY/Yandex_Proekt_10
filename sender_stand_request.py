e49e7417df0670dcddc8ac32e3516f39d47b464e
Your Name <you@example.com>
2023-09-27 14:24:28 +0600

    Повторение 2

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,7 +1,11 @@
 import configuration
 import requests
 import data
-
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+response =get_users_table()
+print(response.status_code)
+print(response.text)
 def post_new_user(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # подставляем полный url
                          json=body,  # тут тело
