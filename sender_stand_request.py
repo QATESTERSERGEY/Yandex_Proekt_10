055a3b844a941b90fbe0d4ebb16e767d44d3c731
Your Name <you@example.com>
2023-09-27 12:17:02 +0600

    Повторение

---
+++ sender_stand_request.py
@@ -0,0 +1,16 @@
+import configuration
+import requests
+import data
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # подставляем полный url
+                         json=body,  # тут тело
+                         headers=data.headers)  # а здесь заголовки
+response = post_new_user(data.user_body);
+print(response.url)
+print(response.status_code)
+print(response.json())
+print(response.headers)
+print(response.text)
+print(response.request)
+print(response.ok)
